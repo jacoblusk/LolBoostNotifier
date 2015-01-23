e1ec4f10408f754c356c9d46f2e29709c557371f
jacoblusk <jacobplusk@gmail.com>
2015-01-22 17:17:56 -0800

    add ability to connect using irc

--- LolBoostNotifier/IrcConnectInfo.cs
+++ LolBoostNotifier/IrcConnectInfo.cs
@@ -1,8 +1,34 @@
-﻿using System;
+﻿using System.IO;
+using IrcDotNet;
 
 namespace LolBoostNotifier {
 	public class IrcConnectInfo {
+		private short port = 6667;
+
+		public IrcUserRegistrationInfo RegistrationInfo { get; set; }
+		public string Hostname { get; set; }
+		public short Port {
+			get { return port; }
+			set { port = value; }
+		}
 		public IrcConnectInfo () {
+		}
+		public static IrcConnectInfo FromFile(string filename) {
+			var info = new IrcConnectInfo ();
+			info.RegistrationInfo = new IrcUserRegistrationInfo ();
+			using (FileStream fs = File.Open (filename, FileMode.Open))
+			using (var reader = new StreamReader (fs)) {
+				short port;
+				info.RegistrationInfo.UserName = reader.ReadLine ();
+				info.RegistrationInfo.NickName = reader.ReadLine ();
+				info.RegistrationInfo.RealName = reader.ReadLine ();
+				info.Hostname = reader.ReadLine ();
+				if (!reader.EndOfStream && short.TryParse (reader.ReadLine (), out port)) {
+					info.Port = port;
+				}
+
+			}
+			return info;
 		}
 	}
 }
--- LolBoostNotifier/Program.cs
+++ LolBoostNotifier/Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
+using System.IO;
+using IrcDotNet;
 
 namespace LolBoostNotifier {
 	class Program {
+		private static IrcClient client;
 		public static void Main (string[] args) {
+			client = new IrcClient ();
+			var connectInfo = IrcConnectInfo.FromFile ("ircinfo.txt");
+			client.FloodPreventer = new IrcStandardFloodPreventer (4, 2000);
+			client.Connected += HandleConnected;
+			client.Registered += HandleRegistered;
+			client.ConnectFailed += HandleConnectFailed;
+			client.ProtocolError += HandleProtocolError;
+			client.Connect (connectInfo.Hostname, connectInfo.Port, false, connectInfo.RegistrationInfo);
+			while (Console.ReadLine () != "quit");
+			client.Disconnect ();
+		}
+
+		static void HandleProtocolError (object sender, IrcProtocolErrorEventArgs e){
+			Console.WriteLine (e.Message);
+		}
+
+		static void HandleConnectFailed (object sender, IrcErrorEventArgs e){
+			Console.WriteLine (e.Error.ToString ());
+		}
+
+		static void HandleJoinedChannel (object sender, IrcChannelEventArgs e) {
+			Console.WriteLine ("joined channel");
 			var lolboost = new LolBoost ();
 			if (lolboost.Login (Credentials.FromFile("creds.txt"))) {
-				lolboost.OnNewBoost += delegate(object sender, NewBoostEventArgs bargs) {
-					Console.WriteLine(bargs.Boost.ToString());
+				lolboost.OnNewBoost += delegate(object sender1, NewBoostEventArgs bargs) {
+					client.LocalUser.SendMessage(e.Channel, bargs.Boost.ToString());
 				};
 				lolboost.Start ();
 			}
+		}
+
+		static void HandleRegistered (object sender, EventArgs e) {
+			Console.WriteLine ("registered");
+			client.LocalUser.JoinedChannel += HandleJoinedChannel;
+		}
+
+		static void HandleConnected (object sender, EventArgs e) {
+			Console.WriteLine ("connected");
+			client.SendRawMessage ("join #boosts");
 		}
 	}
 }
